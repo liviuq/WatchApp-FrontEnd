deafdebd5754cdd041cbc5a0329b4fe073b86fe8
Liviu Andrei <petrache.andrei1@gmail.com>
2022-03-27 12:00:36 +0300

    adjusted the compiler build options

--- src/main/java/ro/info/a3/watchapp/WatchappApplication.java
+++ src/main/java/ro/info/a3/watchapp/WatchappApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WatchappApplication {
+public class WatchappApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(WatchappApplication.class, args);
 	}
 
