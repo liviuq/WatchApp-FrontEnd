08e3c7053c645225bf50103890386b32d97e7b83
Liviu Andrei <petrache.andrei1@gmail.com>
2022-03-27 11:36:37 +0300

    basic spring files

---
+++ src/main/java/ro/info/a3/watchapp/WatchappApplication.java
@@ -0,0 +1,13 @@
+package ro.info.a3.watchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/info/a3/watchapp/WatchappApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.info.a3.watchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
