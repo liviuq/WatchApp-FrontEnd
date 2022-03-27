b42029c82646b0b7fa477fb13bfa943aa48af361
Liviu Andrei <petrache.andrei1@gmail.com>
2022-03-27 15:58:14 +0300

    Test for CI

--- src/test/java/ro/info/a3/watchapp/WatchappApplicationTests.java
+++ src/test/java/ro/info/a3/watchapp/WatchappApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WatchappApplicationTests {
+class WatchappApplicationTests
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
 	}
 
 }
