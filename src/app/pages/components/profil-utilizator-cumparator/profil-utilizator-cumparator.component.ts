9663ffd9bb4c57d7fffc934b241c933dd7454119
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-05-09 11:35:04 +0300

    modal form added for address

--- src/app/pages/components/profil-utilizator-cumparator/profil-utilizator-cumparator.component.ts
+++ src/app/pages/components/profil-utilizator-cumparator/profil-utilizator-cumparator.component.ts
@@ -8,26 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilUtilizatorCumparatorComponent implements OnInit {
 
   public show:boolean = false;
-  public buttonName:any = 'vezi nr*';
   public numberTel:any='+4078*******';
   public numberTelColor:any='#FFFFFF';
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-  getNumber(){
-    
-    this.show = !this.show;
-    if(this.show)  
-      {this.buttonName = "ascunde nr*";
-      this.numberTel= "+40787555239";
-      this.numberTelColor="#FFA500";
-    }
-    else
-      {this.buttonName = "vezi nr*";
-       this.numberTel ="+4078*******";
-       this.numberTelColor="#FFFFFF";
-    }
+  toggle(){
+    this.show=!this.show;
   }
 
 }
