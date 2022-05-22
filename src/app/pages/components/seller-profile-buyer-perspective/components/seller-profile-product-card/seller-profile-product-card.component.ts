a4b438276a7189483dc31b81d93bba2e4eed6dc4
Crekto <hiimcrekto@yahoo.com>
2022-05-22 22:25:34 +0300

    Did routing

--- src/app/pages/components/seller-profile-buyer-perspective/components/seller-profile-product-card/seller-profile-product-card.component.ts
+++ src/app/pages/components/seller-profile-buyer-perspective/components/seller-profile-product-card/seller-profile-product-card.component.ts
@@ -19,6 +19,8 @@ export class SellerProfileProductCardComponent implements OnInit {
   watchPrice!: string;
   @Input()
   watchYear!:string;
+  @Input()
+  productId!:string;
   constructor() { }
 
   ngOnInit(): void {
