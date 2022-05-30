4e5c9e73d0d10ba9a57417f1f0b8b2071b48461e
Alexandru-Cristian Cretu <cretualexandru1221@gmail.com>
2022-05-30 16:22:13 +0300

    Validator

--- src/app/pages/components/form-addproduct/form-addproduct.component.ts
+++ src/app/pages/components/form-addproduct/form-addproduct.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup,FormsModule, ReactiveFormsModule, Validators,FormBuilder,AbstractControl } from '@angular/forms';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
@@ -19,17 +19,18 @@ export class FormAddproductComponent implements OnInit {
   condition_values:any=["Unworn","Mint","Excellent","Very Good","Good","Fair","Poor"];
   carcase_values:any=["Silver","Gold","Steel","White Gold","Ceramics"]
   carcase_color:any=["Silver","Gold","Rose Gold","Neon"];
-
-  step: any = 1;
+  control:any=1;
+  step: any = 3;
   userId :any;
 
   submitted: any = false;
+  
   multistep = new FormGroup({
       itemDetails:new FormGroup({
-        brand: new FormControl('',Validators.required),
-        price: new FormControl('',Validators.required),
+        brand: new FormControl(''),
+        price: new FormControl(''),
         date: new FormControl(''),
-        year: new FormControl('',Validators.required),
+        year: new FormControl(''),
         strap: new FormControl(''),
         glass: new FormControl(''),
         strap_color: new FormControl(''),
@@ -40,13 +41,13 @@ export class FormAddproductComponent implements OnInit {
         carcase_thickness: new FormControl(''),
         alarm: new FormControl(''),
         timer: new FormControl(''),
-        mechanism: new FormControl('',Validators.required),
+        mechanism: new FormControl(''),
         rating: new FormControl(''),
-        gender: new FormControl('',Validators.required),
+        gender: new FormControl(''),
         promoted: new FormControl(''),
         category: new FormControl(''),
-        model: new FormControl('',Validators.required),
-        conditions: new FormControl('',Validators.required)
+        model: new FormControl(''),
+        conditions: new FormControl('')
       }),
 
       userDetails: new FormGroup({
@@ -61,7 +62,17 @@ export class FormAddproductComponent implements OnInit {
     }),
      
   })
-  
+  validation=new FormGroup({
+    brand: new FormControl(''),
+    price: new FormControl(''),
+    year: new FormControl(''),
+    strap: new FormControl(''),
+    strap_color: new FormControl(''),
+    mechanism: new FormControl(''),
+    gender: new FormControl(''),
+    model: new FormControl(''),
+    conditions: new FormControl('')
+  })
 
   
   constructor(public auth: AuthService, private http: HttpClient) { }
@@ -75,7 +86,7 @@ export class FormAddproductComponent implements OnInit {
   })
 
   postData(): void{
-    console.log('dsadasd');
+
     console.log(this.multistep.value.itemDetails);
     this.auth.user$.subscribe(
       (profile) => {
@@ -107,21 +118,54 @@ export class FormAddproductComponent implements OnInit {
       );
 
   }
+  get f(){
+    return this.multistep.value.itemDetails.controls;
+  }
+  
+
+  public findInvalidControls() {
+    var OK=1;
+    const controls = this.validation.value;
+    for (const name in controls) {
+        if (controls[name].length==0) {
+            OK=0; 
+        }
+    }
+    return OK;
+}
 
   
 next() {
   // if(this.multistep.controls['itemDetails'].invalid)
   // return ;
+  // if(this.check()==1)
+  // if(this.multistep.status=="VALID")
+  // this.step = this.step + 1;
+  // else alert("CUCU") ;
+  this.validation.value.brand= this.multistep.value.itemDetails.brand;
+  this.validation.value.price= this.multistep.value.itemDetails.price;
+  this.validation.value.year= this.multistep.value.itemDetails.year;
+  this.validation.value.strap= this.multistep.value.itemDetails.strap;
+  this.validation.value.strap_color= this.multistep.value.itemDetails.strap_color;
+  this.validation.value.mechanism= this.multistep.value.itemDetails.mechanism;
+  this.validation.value.gender= this.multistep.value.itemDetails.gender;
+  this.validation.value.model= this.multistep.value.itemDetails.model;
+  this.validation.value.conditions= this.multistep.value.itemDetails.conditions;
+  console.log(this.validation.controls);
+  console.log(this.validation.value);
+  if(this.findInvalidControls()==0) {alert("Va rugam sa completati campurile obligatorii!"); 
+  this.control=0;  
+}
+else {
   this.step = this.step + 1;
+  this.control=1;
+  }
+  console.log(this.findInvalidControls());
 }
 
 previous() {
 this.step = this.step - 1;
 }
-
-
-
-
 
 }
 
